e083383a78d2c52b5976cf4f88bf820a3e695dbe
BobbyLeeSH <tkdgus1797@gmail.com>
2021-09-14 21:19:39 +0800

    #4 - created empty league page

--- src/const/defaultHeaderMenu.js
+++ src/const/defaultHeaderMenu.js
@@ -1,5 +1,5 @@
 const defaultHeaderMenu = [
-    {title: 'Schedule', path: '/Schedule'}, 
+    {title: 'Schedule', path: '/Schedules'}, 
     {title: 'Leagues', path: '/Leagues'}, 
     {title: 'Leader Board', path: '/LeaderBoard'}, 
     {title: 'Statistics', path: '/Statistics'}, 
---
+++ src/pages/League.js
@@ -0,0 +1,15 @@
+import './League.scss';
+
+const League = () => {
+    return (
+        <div className="league-wrapper">
+            <h3>League Page</h3>
+            <h3>League Page</h3>
+            <h3>League Page</h3>
+            <h3>League Page</h3>
+            League Page
+        </div>
+    )
+}
+
+export default League;
