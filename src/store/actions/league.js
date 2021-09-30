bb4657d610c98b5304adc85252db6dc5a5532cda
BobbyLeeSH <tkdgus1797@gmail.com>
2021-09-30 16:16:03 +0800

    refactored reducer actions

--- src/store/actions/league.js
+++ src/store/actions/league.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const GET_LEAGUES = "GET_LEAGUES";
-export const GET_LEAGUES_SUCCESS = "GET_LEAGUES_SUCCESS";
-export const GET_LEAGUES_FAILURE = "GET_LEAGUES_FAILURE";
 
 export function getLeagues() {
   return (dispatch) => {
@@ -11,7 +9,7 @@ export function getLeagues() {
         "http://alb-sketchbball-dev-1018877792.ap-northeast-2.elb.amazonaws.com/leagues"
       )
       .then((res) => {
-        dispatch({ type: GET_LEAGUES_SUCCESS, data: res.data });
+        dispatch({ type: GET_LEAGUES, data: res.data });
       })
       .catch((err) => {
         // TODO. toast some messages
--- src/store/reducers/league.js
+++ src/store/reducers/league.js
@@ -1,11 +1,11 @@
-import { GET_LEAGUES_SUCCESS } from "../actions/league";
+import { GET_LEAGUES } from "../actions/league";
 const initialState = {
   leagues: [],
 };
 
 export default function leagueReducer(state = initialState, action) {
   switch (action.type) {
-    case GET_LEAGUES_SUCCESS:
+    case GET_LEAGUES:
       return {
         ...state,
         leagues: action.data,
