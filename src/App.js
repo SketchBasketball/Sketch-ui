913584f8f4ce073258bada791ae425c8cf80193d
Sang-Hyeon Bobby Lee <57183358+BobbyLeeSH@users.noreply.github.com>
2021-10-08 17:36:37 +0800

    푸터 레이아웃 (#11) (#13)
    * footer layout only, need to update when information is recieved from Boss
    
    * add footer layout detail
    
    * qa ci
    
    * fix qa ci pipeline
    
    * qa ci test
    
    * qa ci test
    
    * delete qa ci piepline
    
    * max-width to 1080px
    
    * qa distribution id update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import Schedule from "./pages/Schedule";
 import League from "./pages/League";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/schedules" component={Schedule} />
         <Route path="/leagues" component={League} />
       </Switch>
+      <Footer />
     </div>
   );
 }
