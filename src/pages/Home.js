fb5680fa5601df4a7b740cba5ed6cb2a38326c79
BobbyLeeSH <tkdgus1797@gmail.com>
2021-10-09 22:53:35 +0800

    add few lines for schedule line on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,17 @@ import mainTitlesAndRoutes from "../const/mainTitlesAndRoutes";
 function Home() {
   return (
     <div className="home-wrapper">
+      <div className="coming-schedule-wrapper">
+        <div>Upcoming Events In 7 days</div>
+        <div>schedule1</div>
+        <div>schedule2</div>
+        <div>schedule3</div>
+        <div>schedule4</div>
+        <div>schedule5</div>
+        <div>schedule6</div>
+        <div>schedule7</div>
+        <div>schedule8</div>
+      </div>
       <div className="content-wrapper">
         {mainTitlesAndRoutes.map((item, index) => {
           return (
