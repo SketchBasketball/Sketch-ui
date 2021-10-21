0a85170ab64b781ce46f01dcd6bfc7f2a7f1ae62
BobbyLeeSH <tkdgus1797@gmail.com>
2021-10-21 15:47:47 +0800

    home page layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,87 @@
 import React from "react";
 import "./Home.scss";
-import ContentBox from "../components/ContentBox";
-import mainTitlesAndRoutes from "../const/mainTitlesAndRoutes";
 // import { useDispatch, useSelector } from "react-redux";
-// import { getMainSchedules } from "../store/actions/main";
 // import { useEffect } from "react";
-// import Error from "./Error";
-// import Loader from "react-loader-spinner";
-// import HomeScheduleItem from "../components/HomeScheduleItem";
 import HomeSchedule from "../components/HomeSchedule";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import tempImg from "../logo/404.png";
+import YoutubeEmbed from "../components/YoutubeEmbed";
+import {
+  defaultStandingTableHeader,
+  sampleTableData,
+} from "../const/leagueDetailConsts";
 function Home() {
+  const temp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   return (
     <div className="home-wrapper">
       <HomeSchedule />
-
-      <div className="content-wrapper">
-        {mainTitlesAndRoutes.map((item, index) => {
-          return (
-            <ContentBox key={index} title={item.title} path={item.path}>
-              <div>
-                <h3>Home~~~</h3>
-                <h3>Home~~~</h3>
-                <h3>Home~~~</h3>
-                <h3>Home~~~</h3>
-                <h3>Home~~~</h3>
+      <div className="home-carousel-wrapper">
+        <Carousel
+          showArrows={true}
+          infiniteLoop={true}
+          autoPlay={true}
+          showThumbs={false}
+        >
+          {temp.map((item) => {
+            return (
+              <div key={item}>
+                <img
+                  src={tempImg}
+                  className="main-carousel-item"
+                  alt="main-carousel-item"
+                />
               </div>
-            </ContentBox>
-          );
-        })}
+            );
+          })}
+        </Carousel>
+      </div>
+      <HomeSchedule />
+      <div className="content-wrapper">
+        <div className="main-content-box">
+          <div className="main-grid-item-wrapper">
+            <Carousel
+              showArrows={true}
+              infiniteLoop={true}
+              autoPlay={true}
+              showThumbs={false}
+            >
+              {temp.map((item) => {
+                return (
+                  <div className="main-carousel-item" key={item}>
+                    <span>league name</span>
+                    <table className="main-team-standing-table" key={item}>
+                      <thead className="team-standing-table-header">
+                        <tr>
+                          {defaultStandingTableHeader.map((item, index) => {
+                            return <th key={index}>{item}</th>;
+                          })}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {sampleTableData.map((item, index) => {
+                          return (
+                            <tr key={index}>
+                              <td>{item.rank}.</td>
+                              <td className="name">{item.name}</td>
+                              <td>{item.win}</td>
+                              <td>{item.loss}</td>
+                              <td>{item.winRate}</td>
+                              <td>{item.gb}</td>
+                              <td>{item.goalDiff}</td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                );
+              })}
+            </Carousel>
+          </div>
+          <YoutubeEmbed embedId="P5aaWMKD8IQ" />
+        </div>
       </div>
     </div>
   );
