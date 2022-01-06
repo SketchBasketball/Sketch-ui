144d8b812c689058e6f939f265220e30e8701a3d
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-01-06 17:23:01 +0800

    feat: team page on Header

--- src/const/defaultHeaderMenu.js
+++ src/const/defaultHeaderMenu.js
@@ -2,7 +2,7 @@ const defaultHeaderMenu = [
   { title: "Schedule", path: "/Schedules" },
   { title: "Leagues", path: "/Leagues" },
   { title: "Statistics", path: "/Statistics" },
-  // { title: "Teams", path: "/Teams" },
+  { title: "Teams", path: "/Teams" },
   // { title: "Youth Class", path: "/Youth" },
   { title: "Sponsors", path: "/Sponsors" },
 ];
