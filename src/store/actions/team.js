e862385ea709df5a3071ac59bee51f618b556bbc
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-01-18 15:02:46 +0800

    feat: getTeamDetails route

---
+++ src/store/actions/team.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const GET_TEAM_DETAILS = "GET_TEAM_DETAILS";
+export const GET_TEAM_DETAILS_SUCCESS = "GET_TEAM_DETAILS_SUCCESS";
+export const GET_TEAM_DETAILS_FAIL = "GET_TEAM_DETAILS_FAIL";
+
+export function getTeamDetails(id) {
+  return (dispatch) => {
+    dispatch({ type: GET_TEAM_DETAILS });
+    axios
+      .get(`${process.env.REACT_APP_API_PATH}/teams/${id}`)
+      .then((res) => {
+        dispatch({ type: GET_TEAM_DETAILS_SUCCESS, data: res.data });
+      })
+      .catch((err) => {
+        // TODO. toast some messages
+        dispatch({ type: GET_TEAM_DETAILS_FAIL });
+        console.log(err);
+      });
+  };
+}
