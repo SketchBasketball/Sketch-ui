a3e91aa1b02771ef19c31a7a25bc613c702d4f6b
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-01-25 10:33:55 +0800

    feat: TeamStats component

---
+++ src/components/Team/TeamStats.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./TeamStats.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import {
+  overallStatsDefaultHeader,
+  locationStatsDefaultHeader,
+  winlossStatsDefaultHeader,
+} from "../../const/teamDefaultHeader";
+
+const TeamStats = ({ stats }) => {
+  const gridTitle = "Team Statistics";
+
+  console.log(stats);
+
+  return (
+    <div className="team-stats-wrapper">
+      <div className="overall-stats-container">
+        <span>{stats ? gridTitle : "No Team Selected"}</span>
+        <DataGrid
+          rows={stats ? stats : []}
+          columns={overallStatsDefaultHeader}
+          autoHeight={true}
+          rowHeight={30}
+          pageSize={100}
+          rowsPerPageOptions={[100]}
+          hideFooter={true}
+        />
+      </div>
+      <div className="location-stats-container">
+        <DataGrid
+          rows={stats ? stats : []}
+          columns={locationStatsDefaultHeader}
+          autoHeight={true}
+          rowHeight={30}
+          pageSize={100}
+          rowsPerPageOptions={[100]}
+          hideFooter={true}
+        />
+      </div>
+      <div className="winloss-stats-container">
+        <DataGrid
+          rows={stats ? stats : []}
+          columns={winlossStatsDefaultHeader}
+          autoHeight={true}
+          rowHeight={30}
+          pageSize={100}
+          rowsPerPageOptions={[100]}
+          hideFooter={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TeamStats;
