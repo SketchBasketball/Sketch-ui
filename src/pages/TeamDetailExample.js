00cb3c46cb8cad5f73b5d9b66060b9867d834afb
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-01-25 10:32:04 +0800

    feat: teamDetail and teamRoster hard coded data

---
+++ src/pages/TeamDetailExample.js
@@ -0,0 +1,22 @@
+const teamDetails = [
+  {
+    created_at: "2021-11-10T17:48:23",
+    name: "Season 5",
+    end_date: "2021-12-31",
+    updated_at: "2021-11-10T17:48:23",
+    id: "8d3c06a6-9732-4b7b-a4eb-0beae55815f7",
+    start_date: "2021-11-01",
+    league_id: "fb37218d-9ecd-4383-8046-a6f2bc72bd99",
+  },
+  {
+    created_at: "2021-11-10T17:48:22",
+    name: "Season 3",
+    end_date: "2021-12-31",
+    updated_at: "2021-11-10T17:48:22",
+    id: "a156ce88-1c74-4ed4-adef-5e4c8cd01113",
+    start_date: "2021-11-01",
+    league_id: "dfcfb8ef-341c-4d59-b007-ba98e80fa655",
+  },
+];
+
+export default teamDetails;
