c2dd9d717bba2c8b6a876b14e5a570be8defcc4a
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-01-28 19:35:37 +0800

    feat: header banner

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import defaultHeaderMenu from "../../const/defaultHeaderMenu";
 import "./Header.scss";
@@ -7,6 +7,19 @@ import MenuItem from "./MenuItem";
 
 function Header() {
   let history = useHistory();
+  let url = window.location.href
+    .toString()
+    .split(window.location.host)[1]
+    .replace("/", "");
+  const [currentPage, setCurrentPage] = useState(url);
+
+  useEffect(() => {
+    return history.listen((location) => {
+      console.log(`You changed the page to: ${location.pathname}`);
+      setCurrentPage(location.pathname.split("/")[1]);
+      console.log(location);
+    });
+  }, [history]);
 
   return (
     <div>
@@ -22,14 +35,17 @@ function Header() {
           </div>
           <MenuItem key="home" title="Home" path="/" exact={true} />
           {defaultHeaderMenu.map((item, index) => {
-            console.log(item, index);
             return <MenuItem key={index} title={item.title} path={item.path} />;
           })}
         </div>
       </div>
-      <div className="BannerWrapper">
-        <div className="Banner">{}</div>
-      </div>
+      {currentPage ? (
+        <div className="BannerWrapper">
+          <div className="Banner">
+            <span>{currentPage}</span>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
