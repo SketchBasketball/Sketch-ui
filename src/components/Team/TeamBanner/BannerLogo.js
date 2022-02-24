0c41d4211b25130f8873eda57d5d6313eb7a5cf3
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-02-24 18:21:50 +0800

    feat: BannerLogo Component

--- src/components/Team/TeamBanner/BannerLogo.js
+++ src/components/Team/TeamBanner/BannerLogo.js
@@ -1,28 +1,18 @@
 import React from "react";
-import bball from "../../../logo/bball.png";
+import teamLogo from "../../../logo/sketch_logo2.png";
 import "./BannerLogo.scss";
+import { useSelector } from "react-redux";
 
-const BannerLogo = ({ teamDetails }) => {
-  console.log("stats/teamDetails", teamDetails);
-
-  const teamName = teamDetails[0].team_name.toUpperCase();
+const BannerLogo = () => {
+  const { teamDetails } = useSelector((store) => store.teamReducer);
 
   return (
-    <div
-      className="banner-logo-container"
-      style={{ borderStyle: "solid", borderColor: "blue" }}
-    >
-      <div
-        className="banner-logo"
-        style={{ borderStyle: "solid", borderColor: "purple" }}
-      >
-        <img src={bball} className="logo" />
+    <div className="banner-logo-container">
+      <div className="banner-logo">
+        <img src={teamLogo} className="logo" />
       </div>
-      <div
-        className="banner-team-name"
-        style={{ borderStyle: "solid", borderColor: "purple" }}
-      >
-        <div>{teamName}</div>
+      <div className="banner-team-name">
+        <div>{teamDetails[0]?.team_name?.toUpperCase()}</div>
       </div>
     </div>
   );
