79a198e3e16080d4926a7c9df95461088603fc93
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-02-28 16:28:56 +0800

    feat: BannerStaff component

---
+++ src/components/Team/TeamBanner/BannerStaff.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./BannerStaff.scss";
+
+const BannerStaff = () => {
+  return (
+    <div className="banner-staff-container">
+      <div
+        className="banner-staff"
+        style={{ borderRight: "1px solid #ffffff" }}
+      >
+        <div className="staff-title">CAPTAIN</div>
+        <div className="staff-person">-</div>
+      </div>
+
+      <div className="banner-staff">
+        <div className="staff-title">COACH</div>
+        <div className="staff-person">-</div>
+      </div>
+    </div>
+  );
+};
+
+export default BannerStaff;
