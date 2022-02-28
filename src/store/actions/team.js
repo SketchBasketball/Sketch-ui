55cab3b41e8cc85d07b7c07cf521c38928c4816b
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-02-28 14:36:39 +0800

    feat: getAllTimeHighs api

--- src/store/actions/team.js
+++ src/store/actions/team.js
@@ -9,6 +9,10 @@ export const GET_WIN_LOSS_TEAM_DETAILS_SUCCESS =
   "GET_WIN_LOSS_TEAM_DETAILS_SUCCESS";
 export const GET_WIN_LOSS_TEAM_DETAILS_FAIL = "GET_WIN_LOSS_TEAM_DETAILS_FAIL";
 
+export const GET_All_TIME_HIGHS = "GET_All_TIME_HIGHS";
+export const GET_All_TIME_HIGHS_SUCCESS = "GET_All_TIME_HIGHS_SUCCESS";
+export const GET_All_TIME_HIGHS_FAIL = "GET_All_TIME_HIGHS_FAIL";
+
 export function getTeamDetails(id) {
   return (dispatch) => {
     dispatch({ type: GET_TEAM_DETAILS });
@@ -38,6 +42,22 @@ export function getWLTeamDetails(id) {
       .catch((err) => {
         // TODO. toast some messages
         dispatch({ type: GET_WIN_LOSS_TEAM_DETAILS_FAIL });
+        console.log(err);
+      });
+  };
+}
+
+export function getAllTimeHighs(id) {
+  return (dispatch) => {
+    dispatch({ type: GET_All_TIME_HIGHS });
+    axios
+      .get(`${process.env.REACT_APP_API_PATH}/teams/${id}/all-stats/highs`)
+      .then((res) => {
+        dispatch({ type: GET_All_TIME_HIGHS_SUCCESS, data: res.data });
+      })
+      .catch((err) => {
+        // TODO. toast some messages
+        dispatch({ type: GET_All_TIME_HIGHS_FAIL });
         console.log(err);
       });
   };
--- src/store/reducers/team.js
+++ src/store/reducers/team.js
@@ -5,10 +5,14 @@ import {
   GET_WIN_LOSS_TEAM_DETAILS,
   GET_WIN_LOSS_TEAM_DETAILS_SUCCESS,
   GET_WIN_LOSS_TEAM_DETAILS_FAIL,
+  GET_All_TIME_HIGHS,
+  GET_All_TIME_HIGHS_SUCCESS,
+  GET_All_TIME_HIGHS_FAIL,
 } from "../actions/team";
 const initialState = {
   teamDetails: [],
   wlDetails: [],
+  allTimeHighs: [],
   isLoading: true,
   isError: false,
 };
@@ -46,6 +50,24 @@ export default function teamReducer(state = initialState, action) {
         isLoading: false,
       };
     case GET_WIN_LOSS_TEAM_DETAILS_FAIL:
+      return {
+        ...state,
+        isError: true,
+        isLoading: false,
+      };
+    case GET_All_TIME_HIGHS:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case GET_All_TIME_HIGHS_SUCCESS:
+      return {
+        ...state,
+        allTimeHighs: action.data,
+        isError: false,
+        isLoading: false,
+      };
+    case GET_All_TIME_HIGHS_FAIL:
       return {
         ...state,
         isError: true,
