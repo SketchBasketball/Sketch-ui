d3c9a151bd6171eb0d4ead509662b640c93b13e8
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-03-12 14:03:17 +0800

    fix: teamAllTime component updated with allTimeHigh api

--- src/components/Team/TeamAllTime.js
+++ src/components/Team/TeamAllTime.js
@@ -3,35 +3,34 @@ import "./TeamAllTime.scss";
 import { useSelector } from "react-redux";
 
 const TeamAllTime = () => {
-  const { allTimeHighs } = useSelector((store) => store.teamReducer);
-  console.log(allTimeHighs);
+  const { allTimeHigh } = useSelector((store) => store.teamReducer);
 
   return (
     <div className="team-alltime-wrapper">
       <div className="team-alltime-title">{"All Time Records"}</div>
       <div className="team-alltime-records">
         <div className="single-record">
-          <div className="single-record-stat">{allTimeHighs.PTS}</div>
+          <div className="single-record-stat">{allTimeHigh.PTS}</div>
           <div className="single-record-name">PTS</div>
         </div>
         <div className="single-record">
-          <div className="single-record-stat">{allTimeHighs["3PM"]}</div>
+          <div className="single-record-stat">{allTimeHigh["3PM"]}</div>
           <div className="single-record-name">3PM</div>
         </div>
         <div className="single-record">
-          <div className="single-record-stat">{allTimeHighs.REB}</div>
+          <div className="single-record-stat">{allTimeHigh.REB}</div>
           <div className="single-record-name">REB</div>
         </div>
         <div className="single-record">
-          <div className="single-record-stat">{allTimeHighs.AST}</div>
+          <div className="single-record-stat">{allTimeHigh.AST}</div>
           <div className="single-record-name">AST</div>
         </div>
         <div className="single-record">
-          <div className="single-record-stat">{allTimeHighs.STL}</div>
+          <div className="single-record-stat">{allTimeHigh.STL}</div>
           <div className="single-record-name">STL</div>
         </div>
         <div className="single-record">
-          <div className="single-record-stat">{allTimeHighs.BLK}</div>
+          <div className="single-record-stat">{allTimeHigh.BLK}</div>
           <div className="single-record-name">BLK</div>
         </div>
       </div>
