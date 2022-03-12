4a42fd0aa93d7ae6e21e7b6d9fe28af94ba44358
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-03-12 14:05:44 +0800

    fix: TeamBanner components fixed with appropriate apis

--- src/components/Team/TeamBanner.js
+++ src/components/Team/TeamBanner.js
@@ -9,13 +9,17 @@ const TeamBanner = () => {
   return (
     <div className="team-banner-wrapper">
       <div className="team-banner-container">
-        <div className="team-banner-top-container">
-          <BannerLogo />
-          <BannerStats />
+        <div className="team-banner-top-wrapper">
+          <div className="team-banner-top-container">
+            <BannerLogo />
+            <BannerStats />
+          </div>
         </div>
-        <div className="team-banner-bottom-container">
-          <BannerStaff />
-          <BannerBadges />
+        <div className="team-banner-bottom-wrapper">
+          <div className="team-banner-bottom-container">
+            <BannerStaff />
+            <BannerBadges />
+          </div>
         </div>
       </div>
     </div>
