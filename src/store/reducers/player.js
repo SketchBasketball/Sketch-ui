f0c531a2b677bc6e10573393bf8e0f9d8342a327
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-03-19 15:36:48 +0800

    feat: player reducer and api query init commit

---
+++ src/store/reducers/player.js
@@ -0,0 +1,11 @@
+// import { } from "../actions/player ";
+const initialState = {
+  isLoading: true,
+  isError: false,
+};
+export default function playerReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
