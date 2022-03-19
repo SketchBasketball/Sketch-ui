2421021f9edab9c1e5ccb8e0447dbcab55c06a25
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-03-19 18:43:56 +0800

    feat: PlayerBannerBadges Init Commit

---
+++ src/components/Player/PlayerBanner/PlayerBannerBadges.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./PlayerBannerBadges.scss";
+
+const PlayerBannerBadges = () => {
+  return (
+    <div
+      className="player-banner-badges-container"
+      style={{ textAlign: "center" }}
+    >
+      Player Badges Coming Soon!
+      {/* <div className="banner-badges-title">Team Badges</div>
+      <div className="banner-badges-content">TBD</div> */}
+    </div>
+  );
+};
+
+export default PlayerBannerBadges;
