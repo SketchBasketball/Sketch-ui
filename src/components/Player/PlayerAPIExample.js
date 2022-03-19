e0c5846d38a9df0dd974641101227e92531d01c2
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-03-19 18:42:49 +0800

    feat: temporary PlayerAPIExample

---
+++ src/components/Player/PlayerAPIExample.js
@@ -0,0 +1,62 @@
+const playerStats = [
+  {
+    name: "Eric Jee Won Han",
+    profile_photo: null,
+    age: 21,
+    birthdate: "Jan 21st, 1999",
+    weight: 90,
+    height: 195,
+    PPG: 15.7,
+    RPG: 14.8,
+    SPG: 1.9,
+    BPG: 5.7,
+    APG: 11.7,
+    gp: 5,
+    potg: 3,
+  },
+];
+
+const playerCurrentSeasonAvgStats = [
+  {
+    name: "Eric Jee Won Han",
+    league_season_name: "D1 S1",
+    PTS_AVG: 51.2,
+    "3PM_AVG": 11.1,
+    REB_AVG: 13.2,
+    AST_AVG: 15.9,
+    STL_AVG: 4.3,
+    BLK_AVG: 2.9,
+  },
+];
+
+const playerCareerHighStats = [
+  {
+    name: "Eric Jee Won Han",
+    PTS: 81,
+    "3PM": 20,
+    REB: 25,
+    AST: 21,
+    STL: 9,
+    BLK: 8,
+  },
+];
+
+const playerStatsBySeasons = [
+  {
+    name: "Eric Jee Won Han",
+    league_season_name: "D1 S1",
+    PTS: 10,
+    "3PM": 2,
+    REB: 10,
+    AST: 10,
+    STL: 9,
+    BLK: 8,
+  },
+];
+
+export {
+  playerStats,
+  playerCurrentSeasonAvgStats,
+  playerCareerHighStats,
+  playerStatsBySeasons,
+};
