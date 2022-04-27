316ca650558a3e1c464dcc0ae1aba718cd5e705a
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-04-27 20:49:42 +0800

    fix: PlayerDetail fix

--- src/pages/PlayerDetail.js
+++ src/pages/PlayerDetail.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PlayerDetail.scss";
 import { useParams } from "react-router-dom";
-// import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -9,101 +9,102 @@ import InputLabel from "@mui/material/InputLabel";
 import PlayerBanner from "../components/Player/PlayerBanner";
 import PlayerCareerHigh from "../components/Player/PlayerCareerHigh";
 import PlayerCurrentSeasonAvg from "../components/Player/PlayerCurrentSeasonAvg";
+import PlayerMatchStats from "../components/Player/PlayerMatchStats";
 import PlayerSeasonStats from "../components/Player/PlayerSeasonStats";
+import {
+  getPlayerDetails,
+  getPlayerTeams,
+  getPlayerTeamSeasons,
+  getPlayerAllTimeHighs,
+  getPlayerCurrentAVGs,
+  getPlayerMatchStats,
+  getPlayerSeasonStats,
+} from "../store/actions/player";
 
 const PlayerDetail = () => {
   const { playerId } = useParams();
-  console.log(playerId);
-  // const dispatch = useDispatch();
-  // const [playerSeasonId, setPlayerSeasonId] = useState(playerSeasons[0].id);
+  const dispatch = useDispatch();
 
-  const playerSeasons = [
-    {
-      name: "Season 5",
-      id: "64d06da7-dd66-4f63-8f7a-5179904bacbd",
-      start_date: "2021-11-01",
-      end_date: "2021-12-31",
-      league_id: "d413b9e4-465d-4329-a4a0-baf2ec393d87",
-      league_name: "Hong Kong Korean Basketball League",
-    },
-    {
-      name: "Season 4",
-      id: "34d06da7-dd66-4f63-8f7a-5179904bacbd",
-      start_date: "2021-11-01",
-      end_date: "2021-12-31",
-      league_id: "d413b9e4-465d-4329-a4a0-baf2ec393d87",
-      league_name: "Hong Kong Korean Basketball League",
-    },
-  ];
+  const { playerTeams, playerTeamSeasons } = useSelector(
+    (store) => store.playerReducer
+  );
+
+  const [selectedTeam, setSelectedTeam] = useState("");
+  const [selectedSeason, setSelectedSeason] = useState("");
+
+  useEffect(() => {
+    if (playerId) {
+      dispatch(getPlayerDetails(playerId));
+      dispatch(getPlayerTeams(playerId));
+      dispatch(getPlayerAllTimeHighs(playerId));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (playerTeams.length) {
+      setSelectedTeam(playerTeams[0].team_id);
+    }
+  }, [playerTeams]);
+
+  useEffect(() => {
+    if (playerId != "" && selectedTeam != "") {
+      dispatch(getPlayerTeamSeasons(playerId, selectedTeam));
+      dispatch(getPlayerSeasonStats(playerId, selectedTeam));
+    }
+  }, [playerId, selectedTeam]);
+
+  useEffect(() => {
+    if (playerTeamSeasons.length) {
+      setSelectedSeason(playerTeamSeasons[0].season_id);
+    }
+  }, [playerTeamSeasons]);
+
+  useEffect(() => {
+    if (selectedTeam != "" && selectedSeason != "") {
+      dispatch(getPlayerCurrentAVGs(playerId, selectedTeam, selectedSeason));
+      dispatch(getPlayerMatchStats(playerId, selectedTeam, selectedSeason));
+    }
+  }, [playerId, selectedTeam, selectedSeason]);
 
   return (
     <div className="player-wrapper">
       <div className="team-season-search-bar-container">
         <div className="team-season-search-bar">
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-helper-label">
-              {"Leagues"}
-            </InputLabel>
+          <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <InputLabel id="demo-select-small">{"Team"}</InputLabel>
             <Select
-              sx={{ height: "2.5rem" }}
-              labelId="demo-simple-select-helper-label"
-              id="demo-simple-select-helper"
-              value={playerSeasons[0]?.name}
-              label="Leagues"
-              // onChange={(event) => {
-              //   setPlayerSeasonId(event.target.value);
-              // }}
+              labelId="demo-select-small"
+              id="demo-select-small"
+              value={selectedTeam}
+              label="Team"
+              onChange={(event) => {
+                setSelectedTeam(event.target.value);
+              }}
             >
-              {playerSeasons.map((item, index) => {
+              {playerTeams?.map((item, index) => {
                 return (
-                  <MenuItem value={item.id} key={index}>
-                    {item.league_name + " - " + item.name}
+                  <MenuItem value={item.team_id} key={index}>
+                    {item.team_name}
                   </MenuItem>
                 );
               })}
             </Select>
           </FormControl>
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-helper-label">
-              {"Seasons"}
-            </InputLabel>
+          <FormControl sx={{ m: 1, width: 250 }} size="small">
+            <InputLabel id="demo-select-small">{"Leagues/Seasons"}</InputLabel>
             <Select
-              sx={{ height: "2.5rem" }}
-              labelId="demo-simple-select-helper-label"
-              id="demo-simple-select-helper"
-              value={playerSeasons[0]?.name}
-              label="Seasons"
-              // onChange={(event) => {
-              //   setPlayerSeasonId(event.target.value);
-              // }}
+              labelId="demo-select-small"
+              id="demo-select-small"
+              value={selectedSeason}
+              label="Leagues/Seasons"
+              onChange={(event) => {
+                setSelectedSeason(event.target.value);
+              }}
             >
-              {playerSeasons.map((item, index) => {
+              {playerTeamSeasons.map((item, index) => {
                 return (
-                  <MenuItem value={item.id} key={index}>
-                    {item.league_name + " - " + item.name}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-helper-label">
-              {"Teams"}
-            </InputLabel>
-            <Select
-              sx={{ height: "2.5rem" }}
-              labelId="demo-simple-select-helper-label"
-              id="demo-simple-select-helper"
-              value={playerSeasons[0]?.name}
-              label="Teams"
-              // onChange={(event) => {
-              //   setPlayerSeasonId(event.target.value);
-              // }}
-            >
-              {playerSeasons.map((item, index) => {
-                return (
-                  <MenuItem value={item.id} key={index}>
-                    {item.league_name + " - " + item.name}
+                  <MenuItem value={item.season_id} key={index}>
+                    {item.league_name + " - " + item.season_name}
                   </MenuItem>
                 );
               })}
@@ -116,6 +117,7 @@ const PlayerDetail = () => {
         <PlayerCareerHigh />
         <PlayerCurrentSeasonAvg />
       </div>
+      <PlayerMatchStats />
       <PlayerSeasonStats />
     </div>
   );
