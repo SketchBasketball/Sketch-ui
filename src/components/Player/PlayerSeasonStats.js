34dfb452cbb1da36416257362d844c0bd28f5781
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-04-27 20:48:29 +0800

    fix: playerReducer implemented in PlayerSeasonStats

--- src/components/Player/PlayerSeasonStats.js
+++ src/components/Player/PlayerSeasonStats.js
@@ -1,28 +1,24 @@
 import React from "react";
 import "./PlayerSeasonStats.scss";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { DataGrid } from "@mui/x-data-grid";
-import { playerDetailStatisticsHeader } from "../../const/playerDetailStatisticsHeader";
-import { playerStatsBySeasons } from "../Player/PlayerAPIExample";
+import { playerSeasonStatsStatisticsHeader } from "../../const/playerDetailStatisticsHeader";
+// import { playerStatsBySeasons } from "../Player/PlayerAPIExample";
 
 const PlayerSeasonStats = () => {
-  // const { teamPlayerStats } = useSelector((store) => store.teamReducer);
+  const { playerSeasonStats } = useSelector((store) => store.playerReducer);
+  console.log(playerSeasonStats);
 
   return (
     <div className="team-roster-wrapper">
       <div className="team-roster-container">
         <span>
-          {playerStatsBySeasons
-            ? "Player Statistics - " +
-              playerStatsBySeasons[0]?.league_abv.replace(/-/g, " ") +
-              " " +
-              playerStatsBySeasons[0]?.season_name
-            : "No Player Selected"}
+          {playerSeasonStats ? "Season Statistics " : "No Player Selected"}
         </span>
         <DataGrid
-          rows={playerStatsBySeasons ? playerStatsBySeasons : []}
-          columns={playerDetailStatisticsHeader}
-          getRowId={(row) => row.game_id}
+          rows={playerSeasonStats ? playerSeasonStats : []}
+          columns={playerSeasonStatsStatisticsHeader}
+          getRowId={(row) => row.season_id}
           autoHeight={true}
           rowHeight={30}
           pageSize={100}
