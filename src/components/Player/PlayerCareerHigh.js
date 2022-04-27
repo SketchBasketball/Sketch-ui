1755d754cece340a5cdf536c4f6f5ed2344dc42e
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2022-04-27 20:47:01 +0800

    fix: variable names fixed in PlayerCareerHigh

--- src/components/Player/PlayerCareerHigh.js
+++ src/components/Player/PlayerCareerHigh.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./PlayerCareerHigh.scss";
-// import { useSelector } from "react-redux";
-import { playerCareerHighStats } from "../Player/PlayerAPIExample";
+import { useSelector } from "react-redux";
 
 const PlayerCareerHigh = () => {
-  // const { playerCareerHighStats } = useSelector((store) => store.playerReducer);
+  const { playerAllTimeHighs } = useSelector((store) => store.playerReducer);
 
   return (
     <div className="player-careerhigh-wrapper">
@@ -12,37 +11,37 @@ const PlayerCareerHigh = () => {
       <div className="player-careerhigh-records">
         <div className="player-single-record">
           <div className="player-single-record-stat">
-            {playerCareerHighStats[0]?.PTS}
+            {playerAllTimeHighs?.PTS ? playerAllTimeHighs?.PTS : 0}
           </div>
           <div className="player-single-record-name">Points</div>
         </div>
         <div className="player-single-record">
           <div className="player-single-record-stat">
-            {playerCareerHighStats[0]?.["3PM"]}
+            {playerAllTimeHighs?.["3PM"] ? playerAllTimeHighs?.["3PM"] : 0}
           </div>
           <div className="player-single-record-name">3PM</div>
         </div>
         <div className="player-single-record">
           <div className="player-single-record-stat">
-            {playerCareerHighStats[0]?.REB}
+            {playerAllTimeHighs?.REB ? playerAllTimeHighs?.REB : 0}
           </div>
           <div className="player-single-record-name">Rebound</div>
         </div>
         <div className="player-single-record">
           <div className="player-single-record-stat">
-            {playerCareerHighStats[0]?.AST}
+            {playerAllTimeHighs?.AST ? playerAllTimeHighs?.AST : 0}
           </div>
           <div className="player-single-record-name">Assist</div>
         </div>
         <div className="player-single-record">
           <div className="player-single-record-stat">
-            {playerCareerHighStats[0]?.STL}
+            {playerAllTimeHighs?.STL ? playerAllTimeHighs?.STL : 0}
           </div>
           <div className="player-single-record-name">Steal</div>
         </div>
         <div className="player-single-record">
           <div className="player-single-record-stat">
-            {playerCareerHighStats[0]?.BLK}
+            {playerAllTimeHighs?.BLK ? playerAllTimeHighs?.BLK : 0}
           </div>
           <div className="player-single-record-name">Block</div>
         </div>
